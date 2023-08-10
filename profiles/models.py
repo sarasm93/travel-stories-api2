fc32db71571fa5d6c443cf21be1a14d0c4353e28
Sara Smith <saraa.smith@hotmail.com>
2023-08-10 18:37:44 +0000

    Add profiles app, create profile model and connect profile to user

---
+++ profiles/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+
+
+class Profile(models.Model):
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    location = models.CharField(max_length=50)
+    content = models.TextField(max_length=300, blank=True)
+    image = models.ImageField(upload_to='images/', default='../default-profile_pbn3el')
+
+    # Meta och __str__ tagen från Moments API
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"Profile of {self.owner}"
+
+
+# create_profile och kopplingen under tagen från Moments API
+# Create profile automatically when a new user signs up.
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(owner=instance)
+
+# Connect profile to user.
+post_save.connect(create_profile, sender=User)
