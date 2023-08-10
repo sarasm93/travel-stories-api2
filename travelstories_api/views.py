841c6972d254415b2d1d77828f723ea32b3133d5
Sara Smith <saraa.smith@hotmail.com>
2023-08-10 13:50:37 +0000

    Add root route, JSON renderer, pagination and date/time formatting

---
+++ travelstories_api/views.py
@@ -0,0 +1,39 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .settings import (
+    JWT_AUTH_COOKIE, JWT_AUTH_REFRESH_COOKIE, JWT_AUTH_SAMESITE,
+    JWT_AUTH_SECURE,
+)
+
+
+# Kod lånad av Moments projeket - ska du behålla denna referens eller kan du skippa den?????????????????????
+@api_view()
+def root_route(request):
+    return Response({
+        "message": "Welcome to the Travel Stories API!"
+    })
+
+
+# dj-rest-auth logout view - bug fix
+@api_view(['POST'])
+def logout_route(request):
+    response = Response()
+    response.set_cookie(
+        key=JWT_AUTH_COOKIE,
+        value='',
+        httponly=True,
+        expires='Thu, 01 Jan 1970 00:00:00 GMT',
+        max_age=0,
+        samesite=JWT_AUTH_SAMESITE,
+        secure=JWT_AUTH_SECURE,
+    )
+    response.set_cookie(
+        key=JWT_AUTH_REFRESH_COOKIE,
+        value='',
+        httponly=True,
+        expires='Thu, 01 Jan 1970 00:00:00 GMT',
+        max_age=0,
+        samesite=JWT_AUTH_SAMESITE,
+        secure=JWT_AUTH_SECURE,
+    )
+    return response
