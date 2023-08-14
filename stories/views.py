90ccf8e3a70bcac6a3f06a54f4f73475e5e94c81
Sara Smith <saraa.smith@hotmail.com>
2023-08-14 12:59:02 +0000

    Refactor stories views.py and add likes count

--- stories/views.py
+++ stories/views.py
@@ -1,74 +1,34 @@
-from rest_framework import status, permissions
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from django.db.models import Count
+from rest_framework import generics, permissions
+from django_filters.rest_framework import DjangoFilterBackend
+from travelstories_api.permissions import IsOwnerOrReadOnly
 from .models import Story
 from .serializers import StorySerializer
-from django.http import Http404
-from travelstories_api.permissions import IsOwnerOrReadOnly
 
 
-class StoryList(APIView):
+class StoryList(generics.ListCreateAPIView):
     """
-    Travel story list view
+    List stories or create a story if logged in
     """
     serializer_class = StorySerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Story.objects.annotate(
+        likes_count=Count('likes', distinct=True),
+    ).order_by('-created_at')
 
-    def get(self, request):
-        stories = Story.objects.all()
-        serializer = StorySerializer(
-            stories, many=True, context={'request': request}
-        )
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = StorySerializer(
-            data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save(owner=request.user)
-            return Response(
-                serializer.data, status=status.HTTP_201_CREATED
-            )
-        return Response(
-            serializer.errors, status=status.HTTP_400_BAD_REQUEST
-        )
+    def perform_create(self, serializer):
+        """
+        Link a story with the logged in user.
+        """
+        serializer.save(owner=self.request.user)
 
 
-class StoryDetail(APIView):
+class StoryDetail(generics.RetrieveUpdateDestroyAPIView):
     """
-    Story detail view.
-    Render edit form in the browser.
-    Get the story and serialize the data. 
-    If data is valid - save it. Otherwise throw error.
+    Get a story and edit or delete it if you own it.
     """
     serializer_class = StorySerializer
     permission_classes = [IsOwnerOrReadOnly]
-
-    def get_object(self, pk):
-        try:
-            story = Story.objects.get(pk=pk)
-            self.check_object_permissions(self.request, story)
-            return story
-        except Story.DoesNotExist:
-            raise Http404
-    
-    def get(self, request, pk):
-        story = self.get_object(pk)
-        serializer = StorySerializer(story, context={'request': request})
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        story = self.get_object(pk)
-        serializer = StorySerializer(story, data=request.data, context={'request': request})
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        story = self.get_object(pk)
-        story.delete()
-        return Response(
-            status=status.HTTP_204_NO_CONTENT
-        )
+    queryset = Story.objects.annotate(
+        likes_count=Count('likes', distinct=True),
+    ).order_by('-created_at')
