9d9f82098139b9f644ab73ed0e98b07791c04558
Sara Smith <saraa.smith@hotmail.com>
2023-08-14 13:16:53 +0000

    Increase max length of story model content field

--- stories/models.py
+++ stories/models.py
@@ -6,7 +6,7 @@ class Story(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
     destination = models.CharField(max_length=50)
-    content = models.TextField(max_length=300, blank=True)
+    content = models.TextField(max_length=1000, blank=True)
     image = models.ImageField(upload_to='images/', default='../default-image_qzr0l4')
     created_at = models.DateTimeField(auto_now_add=True)
 
