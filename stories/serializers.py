a281ea5b5eae8b5fd38e72db99400f6889cef33e
Sara Smith <saraa.smith@hotmail.com>
2023-08-14 09:55:00 +0000

    Add like id and like counts fields to stories serializer

--- stories/serializers.py
+++ stories/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from stories.models import Story
+from likes.models import Like
 
 
 class StorySerializer(serializers.ModelSerializer):
@@ -7,6 +8,8 @@ class StorySerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    like_id = serializers.SerializerMethodField()
+    likes_count = serializers.ReadOnlyField()
 
     def validate_image(self, value):
         if value.size > 2 * 1024 * 1024:
@@ -24,10 +27,20 @@ class StorySerializer(serializers.ModelSerializer):
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+    
+    def get_like_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            like = Like.objects.filter(
+                owner=user, story=obj
+            ).first()
+            return like.id if like else None
+        return None
 
     class Meta:
         model = Story
         fields = [
             'id', 'owner', 'title', 'destination', 'content', 'image',
             'created_at', 'is_owner', 'profile_id', 'profile_image', 
+            'like_id', 'likes_count',
         ]
