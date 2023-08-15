229b4d8ca208a5f6acc7e6c014df9e4e30a9dccb
Sara Smith <saraa.smith@hotmail.com>
2023-08-15 13:29:25 +0000

    Add story_title methodfield to saves serializers.py

--- saves/serializers.py
+++ saves/serializers.py
@@ -1,6 +1,7 @@
 from django.db import IntegrityError
 from rest_framework import serializers
 from .models import Save
+from .models import Story
 
 
 class SaveSerializer(serializers.ModelSerializer):
@@ -8,12 +9,16 @@ class SaveSerializer(serializers.ModelSerializer):
     Serializer for the Save model.
     """
     owner = serializers.ReadOnlyField(source='owner.username')
+    story_title = serializers.SerializerMethodField()
 
-    class Meta:
-        model = Save
-        fields = [
-            'id', 'owner', 'story', 'created_at'
-        ]
+    def get_story_title(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            save = Save.objects.filter(
+                owner=user, story=obj
+            ).first()
+            return story.title if story else None
+        return None
 
     def create(self, validated_data):
         try:
@@ -22,3 +27,9 @@ class SaveSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError({
                 'detail': 'possible duplicate'
             })
+
+    class Meta:
+        model = Save
+        fields = [
+            'id', 'owner', 'story', 'created_at', 'story_title'
+        ]
--- stories/serializers.py
+++ stories/serializers.py
@@ -54,5 +54,5 @@ class StorySerializer(serializers.ModelSerializer):
         fields = [
             'id', 'owner', 'title', 'destination', 'content', 'image',
             'created_at', 'is_owner', 'profile_id', 'profile_image', 
-            'like_id', 'likes_count', 'comments_count', 'save_id',
+            'like_id', 'likes_count', 'comments_count', 'save_id', 
         ]
