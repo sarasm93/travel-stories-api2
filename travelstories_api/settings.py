c9afa17b5003c4beba0b3e6e76d4179f6237957d
Sara Smith <saraa.smith@hotmail.com>
2023-08-16 08:44:58 +0000

    Change debug to true in settings.py to investigate 500 error

--- travelstories_api/settings.py
+++ travelstories_api/settings.py
@@ -37,7 +37,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEV' in os.environ
+DEBUG = True
+#'DEV' in os.environ
 
 ALLOWED_HOSTS = [
     '8000-sarasm93-travelstoriesa-qevamglw0zx.ws-eu103.gitpod.io', 
