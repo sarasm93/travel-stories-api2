87f17e8d883e42d82d54d5481d5988431e815afb
Sara Smith <saraa.smith@hotmail.com>
2023-08-22 06:29:26 +0000

    Make profiles views with django generic views and add stories_countt field

--- profiles/views.py
+++ profiles/views.py
@@ -1,49 +1,26 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from django.db.models import Count
+from rest_framework import generics, filters
 from .models import Profile
 from .serializers import ProfileSerializer
-from django.http import Http404
-from rest_framework import status
 from travelstories_api.permissions import IsOwnerOrReadOnly
 
 
-class ProfileList(APIView):
+class ProfileList(generics.ListAPIView):
     """
     Profile list view
     """
-    def get(self, request):
-        profiles = Profile.objects.all()
-        serializer = ProfileSerializer(profiles, many=True, context={'request': request})
-        return Response(serializer.data)
-
-
-class ProfileDetail(APIView):
-    """
-    Profile detail view.
-    Render edit form in the browser.
-    Get the profile and serialize the data. 
-    If data is valid - save it. Otherwise throw error.
-    """
     serializer_class = ProfileSerializer
+    queryset = Profile.objects.annotate(
+        stories_count=Count('owner__story', distinct=True),
+    ).order_by('-created_at')
+
+
+class ProfileDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Fetch or edit a profile if you're the owner.
+    """
     permission_classes = [IsOwnerOrReadOnly]
-
-    def get_object(self, pk):
-        try:
-            profile = Profile.objects.get(pk=pk)
-            self.check_object_permissions(self.request, profile)
-            return profile
-        except Profile.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile, context={'request': request})
-        return Response(serializer.data)
-    
-    def put(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile, data=request.data, context={'request': request})
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    serializer_class = ProfileSerializer
+    queryset = Profile.objects.annotate(
+        stories_count=Count('owner__stories', distinct=True),
+    ).order_by('created_at')
