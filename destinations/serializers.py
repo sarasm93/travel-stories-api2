29fa3aa253b69e3c380f955ef04db2efe47cafae
Sara Smith <saraa.smith@hotmail.com>
2023-08-28 19:38:59 +0000

    Remove commented out code in destinations serializers.py

--- destinations/serializers.py
+++ destinations/serializers.py
@@ -7,8 +7,6 @@ class DestinationSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
-    # When using the method field ont he story tag, the user 
-    # can´t select story tags anymore (the form field disappears)
     saved_story_tag = serializers.SerializerMethodField(read_only=True)
 
     def get_is_owner(self, obj):
@@ -20,14 +18,6 @@ class DestinationSerializer(serializers.ModelSerializer):
         for story in obj.story_tag.all():
             story_name.append({"story": story.story.title, "id": story.story.pk})
         return story_name
-
-    # Tried:
-    # story_tag = serializers.ReadOnlyField(source='owner.save.story')
-    # story_tag = serializers.ReadOnlyField(source='story.title')
-    # story_tag = SaveSerializer(read_only=True, many=True)
-
-    # def to_representation(self, value):
-    #    return value.story.title
 
     class Meta:
         model = Destination
