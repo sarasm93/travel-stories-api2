b94109134d97d610c7c2e084f3dec07ebbcb31de
Sara Smith <saraa.smith@hotmail.com>
2023-08-29 21:41:59 +0000

    Add blank=true to profiles location model field

--- profiles/models.py
+++ profiles/models.py
@@ -6,7 +6,7 @@ from django.db.models.signals import post_save
 class Profile(models.Model):
     owner = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
     created_at = models.DateTimeField(auto_now_add=True)
-    location = models.CharField(max_length=50)
+    location = models.CharField(max_length=50, blank=True)
     content = models.TextField(max_length=300, blank=True)
     image = models.ImageField(
         upload_to='images/', default='../default-profile_pbn3el')
